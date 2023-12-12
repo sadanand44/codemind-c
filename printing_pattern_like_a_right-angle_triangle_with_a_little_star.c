14daad6488732ca4cd84e67b83d2bd8610560c09
sadanand44 <153515874+sadanand44@users.noreply.github.com>
2023-12-13 00:06:22 +0530

    #include<stdio.h> int main(){     int a,i;     scanf("%d",&a);     for(i=1;i<=a;i++){         for(int j=1;j<=i;j++){         printf("*");         }          printf(" ");     }
    }

---
+++ printing_pattern_like_a_right-angle_triangle_with_a_little_star.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int a,i;
+    scanf("%d",&a);
+    for(i=1;i<=a;i++){
+        for(int j=1;j<=i;j++){
+        printf("*");
+        }
+         printf("
+");
+    }
+  
+   
+}
