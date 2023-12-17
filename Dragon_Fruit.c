c3cb7f869b5f1f74473fa50b1511d22b9be7141f
sadanand44 <153515874+sadanand44@users.noreply.github.com>
2023-12-17 22:12:39 +0530

    #include<stdio.h> int main(){     int x;     scanf("%d",&x);     if(x==2){         printf("NO");
        }
        else if(x%2==0){
            printf("YES");
            
        }
        else {
            printf("NO");
        }
    }

---
+++ Dragon_Fruit.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int x;
+    scanf("%d",&x);
+    if(x==2){
+        printf("NO");
+        
+    }
+    else if(x%2==0){
+        printf("YES");
+        
+    }
+    else {
+        printf("NO");
+    }
+}
