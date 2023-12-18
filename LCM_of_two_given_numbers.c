5c3bbc9508732a144edf4401f1f9b7944eabd168
sadanand44 <153515874+sadanand44@users.noreply.github.com>
2023-12-18 23:09:07 +0530

    #include<stdio.h> int main(){     int a,b;     scanf("%d%d",&a,&b);     for(int i=a>b?a:b; ;i++){         if(i%a==0&&i%b==0)         {             printf("%d",i);             break;         }
        }
    }

---
+++ LCM_of_two_given_numbers.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    scanf("%d%d",&a,&b);
+    for(int i=a>b?a:b; ;i++){
+        if(i%a==0&&i%b==0) 
+        {
+            printf("%d",i);
+            break;
+        }
+        
+    }
+}
